07bbc8b4e6b6eac3d2dd296a72ab8394c099cbac
Alim Weber <alim.weber@outlook.com>
2024-12-06 17:10:05 +0100

    naive chunk gen and loading

--- index.js
+++ index.js
@@ -26,41 +26,123 @@ let chunk_pos = [0, 0]
 //players position inside of a chunk
 let chunk_coords = [0, 0]
 
+//one cell in every direction making for 8 adjacent tiles
+let render_dist = 1;
+
+//all chunks
+let chunks = new Array(3).fill(new Array(3).fill(map_array))
 
 
+let world_dimensions = [3, 3]
+//this coordinate indicates where the 0, 0 chunk is stored in the chunks array
+//and changes whenever the player adds new rows/columns to the chunks (only on the top and left...)
+let chunk_offset = [1, 1];
 
+//8 chunks surrounding the player + 1 chunks where the player stands in so we dont have to draw the whole chunks array
+let loaded_chunks = []
+
+//takes (and "generates") the chunks adjacent to the player
+//if no such chunks exist (i.e. at the border we generate the chunks)
+function load_chunks(){
+	let loadable_chunks = [];
+	for(let y = -render_dist; y <= render_dist; y++){
+
+		let chunk_row_index = chunk_pos[1] + y + chunk_offset[1];
+
+		if(chunk_row_index < 0){
+			let new_row = new Array(world_dimensions[0]).fill(map_array);
+
+			chunks.unshift(new_row)
+			chunk_offset[1] += 1;
+			world_dimensions[1] += 1;
+
+		}if(chunk_row_index >= world_dimensions[1]){
+			let new_row = new Array(world_dimensions[0]).fill(map_array);
+
+			chunks.push(new_row)
+			world_dimensions[1] += 1;
+		}
+
+		let row = chunks[chunk_row_index];
+
+
+		for(let x = -render_dist; x <= render_dist; x++){
+
+			let chunk_column_index = chunk_pos[0] + x + chunk_offset[0];
+
+			if(chunk_column_index < 0){
+				
+				chunks = chunks.map((chunk, index) => {
+					let new_chunk = chunk;
+					new_chunk.push(map_array)
+					return new_chunk
+				})
+
+				chunk_offset[0] += 1;
+				world_dimensions += 1;
+
+			}
+			if(chunk_column_index >= world_dimensions[0]){
+				chunks = chunks.map((chunk, index) => {
+					let new_chunk = chunk;
+					new_chunk.push(map_array)
+					return new_chunk
+				})
+
+				world_dimensions[0] += 1;
+			}
+			let map_information = row[chunk_column_index];
+
+			let chunk_info = {
+				info : map_information,
+				position : [x + chunk_pos[0], y + chunk_pos[1]]
+			}
+			loadable_chunks.push(chunk_info)
+		}
+	}
+
+	return loadable_chunks;
+}
 
 function create_map(){
-  
-  for(let x = 0; x < chunk_w; x++){
-    for(let y = 0; y < chunk_h; y++){
-      
-      let chunk_index = y * chunk_w + x;
-      
-      let chunk_block = map_array[chunk_index];
-      if(chunk_block == 1){
+	console.log(loaded_chunks, chunk_pos)
+	for(let i = 0; i < loaded_chunks.length; i++){
+		let map_array = loaded_chunks[i].info;
+
+		let offset = loaded_chunks[i].position;
+
+  		for(let x = 0; x < chunk_w; x++){
+    			for(let y = 0; y < chunk_h; y++){
+    
+	    		let x_u = offset[0] * chunk_w + x;
+	    		let y_u = offset[1] * chunk_h + y;
+
+      			let chunk_index = y * chunk_w + x;
+      			let chunk_block = map_array[chunk_index];
+
+      			if(chunk_block == 1){
         
-        let edge1 = [
-          x, y, x + 1, y
-        ]
-        let edge2 = [
-          x + 1, y, x + 1, y + 1
-        ]
-        let edge3 = [
-          x + 1, y + 1, x, y+ 1
-        ]
-        let edge4 = [
-          x, y + 1, x, y
-        ]
-        edges.push(edge1)
-        edges.push(edge2)
-        edges.push(edge3)
-        edges.push(edge4)
-      }
+        		let edge1 = [
+          			x_u, y_u, x_u + 1, y_u
+        		]
+        		let edge2 = [
+          			x_u + 1, y_u, x_u + 1, y_u + 1
+        		]
+        		let edge3 = [
+          			x_u + 1, y_u + 1, x_u, y_u + 1
+        		]
+        		let edge4 = [
+          			x_u, y_u + 1, x_u, y_u
+        		]
+        		edges.push(edge1)
+        		edges.push(edge2)
+        		edges.push(edge3)
+        		edges.push(edge4)
+      		}
       
-    }
+    	}
   }
-  
+} 
 }
 
 function insertIntoSortedArray(sortedArray, value) {
@@ -129,10 +211,6 @@ function walk(){
   }
 
 
-	chunk_pos = [
-		Math.floor(player_pos[0]/chunk_w),
-		Math.floor(player_pos[1]/chunk_h)
-	]
 	
 	let ccx = player_pos[0] % chunk_w;
 	let ccy = player_pos[1] % chunk_h;
@@ -147,7 +225,24 @@ function walk(){
 
 	
 
-	console.log(chunk_pos, chunk_coords)
+	let new_chunk_pos = [
+		Math.floor(player_pos[0]/chunk_w),
+		Math.floor(player_pos[1]/chunk_h)
+	]
+
+	if(chunk_pos[0] == new_chunk_pos[0] && chunk_pos[1] == new_chunk_pos[1]){
+		return
+		chunk_pos = new_chunk_pos
+	}
+
+	
+
+	loaded_chunks = load_chunks();
+	edges = [];
+	create_map();
+
+
+	chunk_pos = new_chunk_pos
 
 }
 
@@ -280,9 +375,15 @@ function render_edges(edges){
   })
 }
 
+
+loaded_chunks = load_chunks();
+
+
 create_map()
 
 let fps = 60;
+
+
 
 setInterval(() => {
 
