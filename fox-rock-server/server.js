e95baadc2b4e68b6837959917297aef3f6870ee4
Alim <alim.weber@outlook.com>
2024-12-17 12:03:22 +0100

    add server creation retreval and deletion as well as self destruction in case of no input

--- fox-rock-server/server.js
+++ fox-rock-server/server.js
@@ -3,22 +3,75 @@ const { GameState } = require("./game_state/game_state");
 const crypto = require("crypto");
 const { HostedServer } = require("./single_server");
 
-const app = require("express")()
+const express = require("express")
+const app = express()
 const server = require("http").createServer(app)
 
+
+app.use(express.json())
 
 const connections = new Map();
 
 
-let hosted = new HostedServer(server, "/");
+let main_server = new HostedServer(server, "");
+
+
+let hosted_servers = new Map()
 
 const updates_per_second = 40
 
 setInterval(() => {
+	//update the main room
+	main_server.update_server();
 
-	hosted.update_server();
+	[...hosted_servers.values()].forEach(hosted_server => {
+		hosted_server.update_server();
+	})
+	
 
 }, 1000/updates_per_second)
+
+
+app.get("/get_rooms", (req, res) => {
+
+	res.send(
+		[...hosted_servers.keys()].map(s => `<div>${s}</div>`).join("")
+	)
+
+})
+
+app.post("/delete_room", (req, res) => {
+	let { server_id } = req.body
+
+	hosted_servers.delete(server_id)
+
+	res.send({
+		success : true
+	})
+})
+
+app.post("/create_room", (req, res) => {
+	
+	let { maybe_path } = req.body
+
+	if(!hosted_servers.has(maybe_path)){
+		let new_server = new HostedServer(server, maybe_path, hosted_servers);
+		hosted_servers.set(maybe_path, new_server)
+
+		return res.send({
+			success : true, 
+			path : maybe_path
+		})
+	}else{
+		let new_server = new HostedServer(server, crypto.randomUUID(), hosted_servers);
+		hosted_servers.set(new_server.id, new_server)
+
+		return res.send({
+			success : true, 
+			path : new_server.id
+		})
+	}
+})
 
 
 server.listen(process.env.PORT || 3000)
--- fox-rock-server/single_server.js
+++ fox-rock-server/single_server.js
@@ -6,11 +6,13 @@ const crypto = require("crypto")
 
 class HostedServer{
 
-	constructor(http_server, id){
-		this.id = id || "/" + crypto.randomUUID();
+	constructor(http_server, id, server_list){
+		this.id = (id ||crypto.randomUUID());
 		this.state = new GameState();
-		this.connection = new WebSocketServer({ path : this.id, server : http_server})
-
+		this.connection = new WebSocketServer({ path :  "/" + this.id, server : http_server})
+		
+		this.server_list = server_list
+		this.last_update = Date.now()
 
 		this.connection_list = new Map();
 
@@ -51,6 +53,7 @@ class HostedServer{
 				if(type == "update"){
 					let { input } = data;
 					this.state.player_input(socket.id, input)
+					this.last_update = Date.now()
 				}
 			})
 		})
@@ -58,6 +61,14 @@ class HostedServer{
 
 
 	update_server(){
+		
+		let now = Date.now();
+
+		if(now - this.last_update > 5 * 1000 && this.server_list){
+			this.server_list.delete(this.id)
+			console.log("i deleted this server because of inactivity")
+		}
+
 		this.state.update();
 		this.broadcast(this.state.serialize())
 	}
